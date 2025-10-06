6244eacc7ee18d5438cbbc8d90b1c1d87013c0a3
Semper Admin <MCESGRosters@aol.com>
2025-10-05 14:37:09 -1000

    Update layout.tsx Removed Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,6 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
-<<<<<<< HEAD
-import { Analytics } from '@vercel/analytics/next';
-=======
->>>>>>> feature/dod-seal-detailed
 
 export const metadata: Metadata = {
   title: 'Marine Corps Directives Formatter',
@@ -26,10 +22,6 @@ export default function RootLayout({
       <body className="font-body antialiased">
         {children}
         <Toaster />
-<<<<<<< HEAD
-        <Analytics />
-=======
->>>>>>> feature/dod-seal-detailed
       </body>
     </html>
   );
